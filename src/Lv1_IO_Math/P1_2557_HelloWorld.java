571cbf9808169c8a4456a253423f82827398904f
JeongMin Yoon <u79jm@koreatech.ac.kr>
2024-06-24 22:44:40 +0900

    240419 solved

--- src/Lv1_IO_Math/P1_2557_HelloWorld.java
+++ src/Lv1_IO_Math/P1_2557_HelloWorld.java
@@ -1,6 +1,6 @@
 package Lv1_IO_Math;
 
-public class HelloWorld {
+public class P1_2557_HelloWorld {
         public static void main(String[] args) {
             System.out.println("Hello World!");
         }
