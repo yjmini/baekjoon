02ea494f565f7ab341e5e3c4f443220c504ffdcb
JeongMin Yoon <u79jm@koreatech.ac.kr>
2024-06-24 22:54:10 +0900

    240624 solved

---
+++ src/Lv1_IO_Math/P2_1000_Plus.java
@@ -0,0 +1,15 @@
+package Lv1_IO_Math;
+
+import java.util.Scanner;
+
+public class P2_1000_Plus {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println(sum);
+    }
+}
