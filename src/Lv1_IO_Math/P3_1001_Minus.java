7be1d678235fd1c9f119fa17118b1bdb3ef05e46
JeongMin Yoon <u79jm@koreatech.ac.kr>
2024-06-24 22:59:27 +0900

    240624 solved

---
+++ src/Lv1_IO_Math/P3_1001_Minus.java
@@ -0,0 +1,16 @@
+package Lv1_IO_Math;
+
+import java.util.Scanner;
+
+public class P3_1001_Minus {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        int result = num1 - num2;
+        System.out.println(result);
+    }
+}
