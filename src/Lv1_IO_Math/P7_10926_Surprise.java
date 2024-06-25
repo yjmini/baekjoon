4682993deb2185f0ac75ba688193d33474ca99e9
JeongMin Yoon <u79jm@koreatech.ac.kr>
2024-06-26 00:36:11 +0900

    240625 solved

---
+++ src/Lv1_IO_Math/P7_10926_Surprise.java
@@ -0,0 +1,14 @@
+package Lv1_IO_Math;
+
+import java.util.Scanner;
+
+public class P7_10926_Surprise {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String name = scanner.nextLine();
+
+        System.out.println(name + "??!");
+    }
+}
