196fac4bef0509fa3237c017c59a3b4982120668
JeongMin Yoon <u79jm@koreatech.ac.kr>
2024-06-27 00:03:53 +0900

    240627 solved

---
+++ src/Lv2_Condition/P1_1330_CompareNum.java
@@ -0,0 +1,20 @@
+package Lv2_Condition;
+
+import java.util.Scanner;
+
+public class P1_1330_CompareNum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+
+        if (num1 < num2) {
+            System.out.println("<");
+        } else if (num1 > num2) {
+            System.out.println(">");
+        } else {
+            System.out.println("==");
+        }
+    }
+}
