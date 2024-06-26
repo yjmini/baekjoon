6319417644cf145f6d9ce8ab0fd848ded9164393
JeongMin Yoon <u79jm@koreatech.ac.kr>
2024-06-26 17:49:53 +0900

    240626 solved

---
+++ src/Lv1_IO_Math/P8_18108_TransYear.java
@@ -0,0 +1,13 @@
+package Lv1_IO_Math;
+
+import java.util.Scanner;
+
+public class P8_18108_TransYear {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int buddhistYear = scanner.nextInt();
+        System.out.println(buddhistYear - 543);
+    }
+}
