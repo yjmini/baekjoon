784f56a5bfaf8ab2ad991ddabbe62ca0d9eefaee
JeongMin Yoon <u79jm@koreatech.ac.kr>
2024-06-29 22:30:01 +0900

    240629 solved

---
+++ src/Lv3_Loop/P1_TimesTable.java
@@ -0,0 +1,15 @@
+package Lv3_Loop;
+
+import java.util.Scanner;
+
+public class P1_TimesTable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < 9; i++) {
+            System.out.println(n + " * " + (i + 1) + " = " + (n * (i + 1)));
+        }
+    }
+}
