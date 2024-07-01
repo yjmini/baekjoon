2d00b22b3e553c5525bd282e764c7786e9aabb88
JeongMin Yoon <u79jm@koreatech.ac.kr>
2024-07-01 19:52:04 +0900

    240629 solved

--- src/Lv3_Loop/P1_2739_TimesTable.java
+++ src/Lv3_Loop/P1_2739_TimesTable.java
@@ -2,7 +2,7 @@ package Lv3_Loop;
 
 import java.util.Scanner;
 
-public class P1_TimesTable {
+public class P1_2739_TimesTable {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
