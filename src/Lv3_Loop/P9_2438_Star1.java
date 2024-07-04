5dc84ab798d84cb9175464ea1b14037698335258
JeongMin Yoon <u79jm@koreatech.ac.kr>
2024-07-04 22:08:53 +0900

    240704 solved

---
+++ src/Lv3_Loop/P9_2438_Star1.java
@@ -0,0 +1,18 @@
+package Lv3_Loop;
+
+import java.util.Scanner;
+
+public class P9_2438_Star1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
