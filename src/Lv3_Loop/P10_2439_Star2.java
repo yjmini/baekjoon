a058198ef21c9d7c39f6613e32708ba3b6becc7d
JeongMin Yoon <u79jm@koreatech.ac.kr>
2024-07-05 15:13:54 +0900

    240705 solved

---
+++ src/Lv3_Loop/P10_2439_Star2.java
@@ -0,0 +1,21 @@
+package Lv3_Loop;
+
+import java.util.Scanner;
+
+public class P10_2439_Star2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < (n - i - 1); j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
