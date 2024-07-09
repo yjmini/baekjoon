d192928ab391b4e3528102e898d3eb981954f5bf
JeongMin Yoon <u79jm@koreatech.ac.kr>
2024-07-09 22:34:44 +0900

    240709 solved

---
+++ src/Lv5_String/P4_11654_ASCII.java
@@ -0,0 +1,14 @@
+package Lv5_String;
+
+import java.util.Scanner;
+
+public class P4_11654_ASCII {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String X = scanner.nextLine();
+        int n = (int) X.charAt(0);
+
+        System.out.println(n);
+    }
+}
