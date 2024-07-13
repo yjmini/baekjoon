2807bffd362391d4de66a24162a4c9fa1b4fb918
JeongMin Yoon <u79jm@koreatech.ac.kr>
2024-07-13 16:00:33 +0900

    240713 solved

---
+++ src/Lv5_String/P11_11718_CopyPrint.java
@@ -0,0 +1,15 @@
+package Lv5_String;
+
+import java.util.Scanner;
+
+public class P11_11718_CopyPrint {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextLine()) {
+            String str = scanner.nextLine();
+
+            System.out.println(str);
+        }
+    }
+}
