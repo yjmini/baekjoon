51378e1bd2f8b2393280745df9cb5f00891fc0b9
JeongMin Yoon <u79jm@koreatech.ac.kr>
2024-07-16 20:53:27 +0900

    240716 solved

---
+++ src/Lv6_Advanced1/P0_10699_TodayDate.java
@@ -0,0 +1,16 @@
+package Lv6_Advanced1;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class P0_10699_TodayDate {
+    public static void main(String[] args) {
+        Date date = new Date();
+        DateFormat df = new SimpleDateFormat("YYYY-MM-dd");
+        df.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
+
+        System.out.println(df.format(date));
+    }
+}
