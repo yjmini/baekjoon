08ef3a9d4e92366e4cce5cf035eecf9ac19e3646
JeongMin Yoon <u79jm@koreatech.ac.kr>
2024-08-05 23:57:42 +0900

    240805 solved

---
+++ src/Marathon/P12_5338_MSLogo.java
@@ -0,0 +1,13 @@
+package Marathon;
+
+
+public class P12_5338_MSLogo {
+    public static void main(String[] args) {
+        System.out.println(
+                "       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+    }
+}
