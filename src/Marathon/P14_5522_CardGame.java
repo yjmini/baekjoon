60334c7547f5ace3b21b598a91036464a84c4d50
JeongMin Yoon <u79jm@koreatech.ac.kr>
2024-08-06 11:01:34 +0900

    240806 solved

---
+++ src/Marathon/P14_5522_CardGame.java
@@ -0,0 +1,17 @@
+package Marathon;
+
+import java.util.Scanner;
+
+public class P14_5522_CardGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a1 = scanner.nextInt();
+        int a2 = scanner.nextInt();
+        int a3 = scanner.nextInt();
+        int a4 = scanner.nextInt();
+        int a5 = scanner.nextInt();
+
+        System.out.println(a1 + a2 + a3 + a4 + a5);
+    }
+}
