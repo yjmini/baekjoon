d85c12d52477dfc1361ac47195662bb29abb32aa
JeongMin Yoon <u79jm@koreatech.ac.kr>
2024-08-07 23:12:58 +0900

    240807 solved

---
+++ src/Marathon/P15_5337_Welcome.java
@@ -0,0 +1,10 @@
+package Marathon;
+
+public class P15_5337_Welcome {
+    public static void main(String[] args) {
+        System.out.println(
+                ".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
