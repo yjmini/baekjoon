1d0d9aea88c6a624fa7a16e0dbd8b0d90530c15d
JeongMin Yoon <u79jm@koreatech.ac.kr>
2024-08-08 11:20:24 +0900

    240808 solved

---
+++ src/Marathon/P17_16394_HongikUniv.java
@@ -0,0 +1,14 @@
+package Marathon;
+
+import java.util.Scanner;
+
+public class P17_16394_HongikUniv {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int currentYear = scanner.nextInt();
+        int schoolOpenYear = 1946;
+
+        System.out.println(currentYear - schoolOpenYear);
+    }
+}
