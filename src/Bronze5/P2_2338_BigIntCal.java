a686a577090ec288da2ea733502f8ece7fd816f5
JeongMin Yoon <u79jm@koreatech.ac.kr>
2024-08-13 01:30:45 +0900

    240812 solved

---
+++ src/Bronze5/P2_2338_BigIntCal.java
@@ -0,0 +1,17 @@
+package Bronze5;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class P2_2338_BigIntCal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        BigInteger a = scanner.nextBigInteger();
+        BigInteger b = scanner.nextBigInteger();
+
+        System.out.println(a.add(b));
+        System.out.println(a.subtract(b));
+        System.out.println(a.multiply(b));
+    }
+}
