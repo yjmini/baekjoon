8f76615db7023655c372a344a926fdd2c9c4d243
JeongMin Yoon <u79jm@koreatech.ac.kr>
2024-08-16 05:37:08 +0900

    240814 solved

---
+++ src/Bronze5/P4_2558_APlusB2.java
@@ -0,0 +1,14 @@
+package Bronze5;
+
+import java.util.Scanner;
+
+public class P4_2558_APlusB2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(a + b);
+    }
+}
