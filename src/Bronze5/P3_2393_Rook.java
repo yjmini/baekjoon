eac325c33b49530a0ba824df6ce7e7d9f135dfd0
JeongMin Yoon <u79jm@koreatech.ac.kr>
2024-08-16 05:36:04 +0900

    240813 solved

---
+++ src/Bronze5/P3_2393_Rook.java
@@ -0,0 +1,19 @@
+package Bronze5;
+
+public class P3_2393_Rook {
+    public static void main(String[] args) {
+        System.out.println("  ___  ___  ___");
+        System.out.println("  | |__| |__| |");
+        System.out.println("  |           |");
+        System.out.println("   \\_________/");
+        System.out.println("    \\_______/");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |_____|");
+        System.out.println("  __/       \\__");
+        System.out.println(" /             \\");
+        System.out.println("/_______________\\");
+    }
+}
