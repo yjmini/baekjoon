9df1e693013876acfee5049d3ea1f35864fcc70c
JeongMin Yoon <u79jm@koreatech.ac.kr>
2024-08-27 06:18:29 +0900

    240818 solved

---
+++ src/Bronze5/P7_5339_C3PO.java
@@ -0,0 +1,18 @@
+package Bronze5;
+
+
+public class P7_5339_C3PO {
+    public static void main(String[] args) {
+        System.out.println(
+                "     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+    }
+}
