410df11c9130bcc293886fa04a7c153eb4f03ed9
JeongMin Yoon <u79jm@koreatech.ac.kr>
2024-09-08 21:56:36 +0900

    240908 solved

---
+++ src/Bronze5/P14_10189_Hook.java
@@ -0,0 +1,11 @@
+package Bronze5;
+
+public class P14_10189_Hook {
+    public static void main(String[] args) {
+        System.out.println(
+                "#  # #### #### #  #\n" +
+                "#### #  # #  # # #\n" +
+                "#### #  # #  # # #\n" +
+                "#  # #### #### #  #");
+    }
+}
