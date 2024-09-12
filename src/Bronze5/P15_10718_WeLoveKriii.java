47ee47da5a9fc83a35eb9d928c6bf71a10746603
JeongMin Yoon <u79jm@koreatech.ac.kr>
2024-09-12 23:46:32 +0900

    240912 solved

---
+++ src/Bronze5/P15_10718_WeLoveKriii.java
@@ -0,0 +1,10 @@
+package Bronze5;
+
+public class P15_10718_WeLoveKriii {
+
+    public static void main(String[] args) {
+        System.out.println(
+                "강한친구 대한육군\n" +
+                "강한친구 대한육군");
+    }
+}
